c99e87f1634da7b411a428632dbc690038c0ba3e
Alexander Presber <post@momolog.info>
2012-06-06 09:24:33 +0200

    clean up PHP code

--- mecode.php
+++ mecode.php
@@ -1,145 +1,129 @@
 <?php
-include("utils.php");
+  include("utils.php");
 
-//config
-$client_url = 'http://reecode.eu';
+  # config
+  # FIXME: config should go to config.yaml
+  function config($key){
+    $config = array(
+      'client_url'  => 'http://reecode.eu',
+      'me_url'      => 'http://mecode.momolog.info'
+      # 'me_url'    => 'http://localhost:3000'
+    );
+    return @$config[$key];
+  }
 
-$me_url = 'http://mecode.momolog.info';
-# $me_url = 'http://localhost:3000';
+  # code
+  # FIXME: global vars are evil!
+  $is_code_invalid  = false;
+  $code             = '';
+  $email            = '';
 
-$products = array(
-    "2"
-//, "2"
-);
+  _init();
 
-
-//code
-$is_code_invalid = false;
-$code = '';
-$email = '';
-_before();
-
-function _before()
-{
-    GLOBAL $is_code_invalid, $me_url, $client_url, $code, $email;
+  function _init() {
+    GLOBAL $is_code_invalid, $code, $email;
     $email = $_REQUEST['email'];
     if ($action = $_REQUEST['action']) {
-
-        if ($action == 'a_recieve') {
-            if ($code = $_REQUEST['code']) {
-                $product = json_from("$me_url/codes/$code/use.json?email=$email");
-                if ($product->reason == 'used' || $product->reason == 'unknown') {
-                    $is_code_invalid = $product->reason;
-                }
-                else {
-                    $code = '';
-                    header("Location: $product->url");
-                }
-            }
+      if ($action == 'a_recieve') {
+        if ($code = $_REQUEST['code']) {
+          $product = json_from("{config('me_url')}/codes/$code/use.json?email=$email");
+          if ($product->reason == 'used' || $product->reason == 'unknown') {
+            $is_code_invalid = $product->reason;
+          } else {
+            $code = '';
+            header("Location: $product->url");
+          }
         }
+      } elseif ($action == 'a_checkout') {
+        if (isset($_REQUEST['invoice']) && isset($_REQUEST['sign'])) {
+          $invoice = urlencode($_REQUEST['invoice']);
+          $signature = urlencode($_REQUEST['sign']);
 
-        elseif ($action == 'a_checkout') {
-            if (isset($_REQUEST['invoice']) && isset($_REQUEST['sign'])) {
-                $invoice = urlencode($_REQUEST['invoice']);
-                $signature = urlencode($_REQUEST['sign']);
-
-                $product = json_from("$me_url/sales/$invoice/$signature/checkout.json");
-                header("Location: $product->url");
-            }
+          $product = json_from(config('me_url')."/sales/$invoice/$signature/checkout.json");
+          header("Location: $product->url");
         }
+      } elseif ($action == 'a_pay'){
+        $p_id = $_REQUEST['product'];
+        $f = json_from(config('me_url')."/products/$p_id/form.json?return_url=".config('client_url')."&email=$email");
 
-        elseif ($action == 'a_pay'){
-            $p_id = $_REQUEST['product'];
-            $f = json_from("$me_url/products/$p_id/form.json?return_url=$client_url&email=$email");
-
-            die("
-                  <form class='topaypal' action='$f->action'>
-                    $f->hidden_inputs
-                    <input name='reeceive' type='submit' value='to Paypal ...' />
-                    $me_url/products/$p_id/form.json?return_url=$client_url&email=$email
-                  </form>
-            ");
-        }
-    }
-
-}
-
-function meheader()
-{
-    echo "
-        <script type='text/javascript' src='mecode/vars.js.php'></script>
-        <script type='text/javascript' src='mecode/vendor.js'></script>
-        <script type='text/javascript' src='mecode/mecode.js'></script>
-
-        <link   type='text/css'       href='mecode/boxy/boxy.css' rel='stylesheet'  />
-        <link   type='text/css'       href='mecode/mecode.css' rel='stylesheet'  />
-    ";
-}
-
-//codes
-function mecode()
-{
-    GLOBAL $is_code_invalid, $code;
-    echo "<div class='mecode'>";
-    if ($is_code_invalid) {
-        $err_msg = array(
-            'used' => 'schade! somebody ate the cake before!',
-            'unknown' => "I don't know about this code. sorry!"
-        );
-        echo "<label class='err'>$err_msg[$is_code_invalid]</label>";
-    }
-
-    echo  "
-          <form action='' method='post'>
-              <table cellpadding='0' cellspacing='0' border='0'>
-                    <tr><td class='textwhite'>reesponse code</td>
-                    </tr>
-                    <tr><td>
-                        <input name='code' class='reesponse code' type='text' value='$code' size='11' maxlength='50' />
-                        <input name='action' type='hidden' value='a_recieve' size='11' maxlength='50' />
-
-                    </td> </tr>
-                    <tr><td class='textwhite' height='17' valign='bottom'>email address</td>
-                    </tr>
-                    <tr><td><input name='email' type='text' value='$email' size='25' maxlength='50' /></td>
-                    </tr>
-                    <tr><td align='right'><img src='img/1x1_trans.gif' width='1' height='5' alt='' border='0' /><br />
-                            <input name='reeceive' type='submit' value='reeceive...!' /></td>
-                    </tr>
-                </table>
-            </form>
-            ";
-    echo "</div>";
-}
-
-
-//products
-function meproduct($p_id)
-{
-    echo  "
-          <form class='me-product'>
-            <table cellpadding='0' cellspacing='0' border='0'>
-                <tr><td class='textwhite' height='17' valign='bottom'>email address</td>
-                </tr>
-                <tr><td>
-                    <input name='action' type='hidden' value='a_pay' />
-                    <input name='product' type='hidden' value='$p_id'/>
-                    <input name='email' type='text' value='$email' size='25' maxlength='50' />
-               </td> </tr>
-                <tr><td align='right'><img src='img/1x1_trans.gif' width='1' height='5' alt='' border='0' /><br />
-                        <input name='reeceive' type='submit' value='Pay ...' /></td>
-                </tr>
-            </table>
+        die("
+          <form class='topaypal' action='$f->action'>
+            $f->hidden_inputs
+            <input name='reeceive' type='submit' value='to Paypal ...' />
+            ".config('me_url')."/products/$p_id/form.json?return_url=".config('client_url')."&email=$email
           </form>
-    ";
-}
-
-;
-function meproducts($products)
-{
-    foreach ($products as $p) {
-        me_product($p);
+        ");
+      }
     }
-}
+  }
 
-?>
+  function meheader() {
+    echo "
+      <script type='text/javascript' src='mecode/vars.js.php'></script>
+      <script type='text/javascript' src='mecode/vendor.js'></script>
+      <script type='text/javascript' src='mecode/mecode.js'></script>
+
+      <link   type='text/css'       href='mecode/boxy/boxy.css' rel='stylesheet'  />
+      <link   type='text/css'       href='mecode/mecode.css' rel='stylesheet'  />
+    ";
+  }
+
+  # codes
+  function mecode() {
+    GLOBAL $is_code_invalid, $code;
+
+    echo "<div class='mecode'>";
+
+    if ($is_code_invalid) {
+      $err_msg = array(
+        'used' => 'schade! somebody ate the cake before!',
+        'unknown' => "I don't know about this code. sorry!"
+      );
+      echo "<label class='err'>$err_msg[$is_code_invalid]</label>";
+    }
+
+    echo  "
+      <form action='' method='post'>
+        <table cellpadding='0' cellspacing='0' border='0'>
+          <tr><td class='textwhite'>reesponse code</td>
+          </tr>
+          <tr><td>
+            <input name='code' class='reesponse code' type='text' value='$code' size='11' maxlength='50' />
+            <input name='action' type='hidden' value='a_recieve' size='11' maxlength='50' />
+          </td> </tr>
+          <tr><td class='textwhite' height='17' valign='bottom'>email address</td>
+          </tr>
+          <tr><td><input name='email' type='text' value='$email' size='25' maxlength='50' /></td>
+          </tr>
+          <tr><td align='right'><img src='img/1x1_trans.gif' width='1' height='5' alt='' border='0' /><br />
+                  <input name='reeceive' type='submit' value='reeceive...!' /></td>
+          </tr>
+        </table>
+      </form>
+    ";
+    echo "</div>";
+  }
+
+  # products
+  function meproduct($p_id) {
+    echo  "
+      <form class='me-product'>
+        <table cellpadding='0' cellspacing='0' border='0'>
+          <tr><td class='textwhite' height='17' valign='bottom'>email address</td>
+          </tr>
+          <tr><td>
+            <input name='action' type='hidden' value='a_pay' />
+            <input name='product' type='hidden' value='$p_id'/>
+            <input name='email' type='text' value='$email' size='25' maxlength='50' />
+          </td> </tr>
+          <tr><td align='right'><img src='img/1x1_trans.gif' width='1' height='5' alt='' border='0' /><br />
+            <input name='reeceive' type='submit' value='Pay ...' /></td>
+          </tr>
+        </table>
+      </form>
+    ";
+  };
+
+  function meproducts($products) {
+    foreach ($products as $p) { meproduct($p); }
+  }
